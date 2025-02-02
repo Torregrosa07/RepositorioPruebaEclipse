fb7fcd44b7b644a511b9b04459e55883b91a5328
Santiago Torregrosa <santiago.torre07a@gmail.com>
2025-02-02 16:48:02 +0100

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Ejemplo de prueba de control de versiones con Git");
+		System.out.println("Commit de prueba");
+		
+		String Commit;
+	}
+
+}
